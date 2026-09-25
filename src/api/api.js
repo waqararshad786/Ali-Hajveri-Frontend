b897bf29156b99ff735db5e6328ec75c9c8f95c6
waqararshad786 <waqar321arshad@gmail.com>
2026-09-25 05:38:21 +0000

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 // src/api/api.js
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+// export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+export const API_URL = import.meta.env.VITE_API_URL;
 
 /* ============================================================
    API ENDPOINTS
